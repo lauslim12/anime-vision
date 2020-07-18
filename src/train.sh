7a21063227612dc84e2917216e0dac577f361d71
lauslim12 <nicholasdwiarto@yahoo.com>
2020-07-18 10:46:17 +0700

    Improved accuracy for the model testing script

--- src/train.sh
+++ src/train.sh
@@ -1,1 +1,1 @@
-python retrain.py --how_many_training_steps 500 --output_graph=./retrained_graph.pb --output_labels=./retrained_labels.txt --image_dir=./../dataset/train/
+python retrain.py --output_graph=./retrained_graph.pb --output_labels=./retrained_labels.txt --image_dir=./../dataset/train/
