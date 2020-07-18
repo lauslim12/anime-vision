f0923571aacb40e2e77974cf6a235ab26fa5c417
lauslim12 <nicholasdwiarto@yahoo.com>
2020-07-18 11:22:57 +0700

    Added arguments for IBM Cloud script

--- src/ibm-cloud.py
+++ src/ibm-cloud.py
@@ -1,7 +1,7 @@
 # Native Python Modules
-import os
 import json
-from zipfile import ZipFile
+import os
+import sys
 
 # Third party Python modules
 from dotenv import load_dotenv
@@ -32,9 +32,18 @@ def create_custom_classifier(visual_recognition):
 def get_classifiers(visual_recognition):
   classifiers = visual_recognition.list_classifiers(verbose=True).get_result()
   print(json.dumps(classifiers, indent=2))
-  classifier_id = classifiers['classifiers'][0]['classifier_id']
+  
+  # Error handling to prevent using an empty list.
+  if not classifiers['classifiers']:
+    sys.exit('Your IBM Cloud Account has no classifiers! Please ensure that you have one!')
+  else:
+    classifier_id = classifiers['classifiers'][0]['classifier_id']
 
   return classifier_id
+
+def delete_classifier(visual_recognition, classifier_id):
+  classifiers = visual_recognition.delete_classifier(classifier_id).get_result()
+  print(json.dumps(classifiers, indent = 2))
 
 def test_classifier(visual_recognition, classifier_id):
   with open('./../dataset/test/erza-test.jpg', 'rb') as image:
@@ -49,13 +58,26 @@ def main():
   # Change directory back to the root folder. Important for easier navigation!
   os.chdir('../')
 
-  # Load environment variables.
+  # Load environment variables and visual recognition.
   load_dotenv()
-
   visual_recognition = authenticate_watson()
-  create_custom_classifier(visual_recognition)
-  classifier_id = get_classifiers(visual_recognition)
-  test_classifier(visual_recognition, classifier_id)
+
+  # Print out error message if the user does not use arguments.
+  if len(sys.argv) != 2:
+    sys.stderr.write("Usage: python ibm-cloud.py <action> (check, delete, create, test) \n")
+    sys.exit(-1)
+
+  # Do process.
+  if sys.argv[1] == 'delete':
+    classifer_id = get_classifiers(visual_recognition)
+    delete_classifier(visual_recognition, classifer_id)
+  elif sys.argv[1] == 'create':
+    create_custom_classifier(visual_recognition)
+  elif sys.argv[1] == 'test':
+    classifier_id = get_classifiers(visual_recognition)
+    test_classifier(visual_recognition, classifier_id)
+  elif sys.argv[1] == 'check':
+    classifer_id = get_classifiers(visual_recognition)
 
 if __name__ == '__main__':
   main()
