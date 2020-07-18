e0d41fadf77444c10162bbd8285af9ddb9803b5f
lauslim12 <nicholasdwiarto@yahoo.com>
2020-07-18 10:45:57 +0700

    Refactored the code to be more cleaner

--- src/ibm-cloud.py
+++ src/ibm-cloud.py
@@ -1,60 +1,12 @@
 # Native Python Modules
 import os
-import sys
 import json
-import glob
 from zipfile import ZipFile
 
 # Third party Python modules
-import cv2
 from dotenv import load_dotenv
 from ibm_watson import VisualRecognitionV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
-# Load Environment Variables
-env_path = './../.env'
-load_dotenv(dotenv_path = env_path)
-
-# Global Constants
-HEIGHT = 100
-WIDTH = 100
-
-# Conversion using Haar Cascade Algorithm
-def conversion(cascade_file="lbpcascade_animeface.xml"):
-  # 1) Create a cascade file filled with the trained classifier from Nagadomi.
-  cascade = cv2.CascadeClassifier(cascade_file)
-  raw_images = os.listdir('./../dataset/raw')
-
-  # 2) Loop through the dataset/raw folder
-  for raw_image in raw_images:
-    # 3) Resize the image to be 100 x 100.
-    # First, read the image, then convert them into greyscale for easier computing.
-    image = cv2.imread(os.path.join('./../dataset/raw', raw_image))
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.equalizeHist(gray)
-
-    # Recognize the face in the image.
-    faces = cascade.detectMultiScale(gray,
-                                     scaleFactor=1.1,
-                                     minNeighbors=5,
-                                     minSize=(25, 25))
-
-    # Iterate through the face to crop.
-    for (x, y, w, h) in faces:
-      # crop_image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-      crop_image = image[y:y + h, x:x + w]
-      filename = os.path.basename(raw_image).split('.')[0]
-      cv2.imwrite(os.path.join('./../dataset/train', filename + ".jpg"),
-                  crop_image)
-
-# Resize all of the images to be 100 x 100.
-def resize():
-  files = os.listdir('./../dataset/train')
-  for file in files:
-    image = cv2.imread(os.path.join('./../dataset/train', file))
-    resized_image = cv2.resize(image, (WIDTH, HEIGHT), interpolation = cv2.INTER_AREA)
-    filename = os.path.basename(file).split('.')[0]
-    cv2.imwrite(os.path.join('./../dataset/train', filename + ".jpg"), resized_image)
 
 # Testing response from IBM Watson.
 def authenticate_watson():
@@ -68,6 +20,7 @@ def authenticate_watson():
   return visual_recognition
 
 # Create custom classifier from processed images.
+# Work in progress!
 def create_custom_classifier(visual_recognition):
   with open('erza.zip', 'rb') as erza, open('jeanne.zip', 'rb') as jeanne:
     model = visual_recognition.create_classifier('erza_scarlet', 
@@ -93,10 +46,14 @@ def test_classifier(visual_recognition, classifier_id):
   print(json.dumps(classes, indent = 2))
 
 def main():
-  # conversion()
-  # resize()
+  # Change directory back to the root folder. Important for easier navigation!
+  os.chdir('../')
+
+  # Load environment variables.
+  load_dotenv()
+
   visual_recognition = authenticate_watson()
-  # create_custom_classifier(visual_recognition)
+  create_custom_classifier(visual_recognition)
   classifier_id = get_classifiers(visual_recognition)
   test_classifier(visual_recognition, classifier_id)
 
---
+++ src/preparation.py
@@ -0,0 +1,61 @@
+# Data preparation
+## Description...
+
+# Native Python Modules
+import os
+
+# Third Party Python Modules
+import cv2
+
+# Global Constants
+WIDTH = 100
+HEIGHT = 100
+
+def conversion(character_name, cascade_file = os.path.join(os.getcwd(), 'models', 'lbpcascade_animeface.xml')):
+  # 1) Create a cascade file filled with the trained classifier from Nagadomi.
+  cascade = cv2.CascadeClassifier(cascade_file)
+  raw_images = os.listdir(os.path.join('dataset', 'raw', character_name))
+
+  # 2) Loop through the dataset/raw folder
+  for raw_image in raw_images:
+    # 3) Resize the image to be 100 x 100.
+    # First, read the image, then convert them into greyscale for easier computing.
+    image = cv2.imread(os.path.join('dataset', 'raw', raw_image))
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
+
+    # Recognize the face in the image.
+    faces = cascade.detectMultiScale(gray,
+                                     scaleFactor=1.1,
+                                     minNeighbors=5,
+                                     minSize=(25, 25))
+
+    # Iterate through the face to crop.
+    for (x, y, w, h) in faces:
+      # crop_image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+      crop_image = image[y:y + h, x:x + w]
+      filename = os.path.basename(raw_image).split('.')[0]
+      cv2.imwrite(os.path.join('dataset', 'train', character_name, filename + '.jpg'), crop_image)
+
+def resize(character_name):
+  files = os.listdir(os.path.join('dataset', 'train', character_name))
+
+  for file in files:
+    image = cv2.imread(os.path.join('dataset', 'train', character_name, file))
+    resized_image = cv2.resize(image, (WIDTH, HEIGHT), interpolation = cv2.INTER_AREA)
+    filename = os.path.basename(file).split('.')[0]
+    cv2.imwrite(os.path.join('dataset', 'train', filename, '.jpg'), resized_image)
+
+def main():
+  if len(sys.argv) != 1:
+    sys.stderr.write("Usage: python preparation.py <character_name> (character name must be a folder inside the 'raw' and 'train' folder, and must not contain spaces!) \n")
+    sys.exit(-1)
+
+  # Change directory back to the root folder. Important for easier navigation!
+  os.chdir('../')
+  
+  conversion(sys.argv[1])
+  resize(sys.argv[1])
+
+if __name__ == "__main__":
+  main()
