673a51ad8c78b78168a97cc84d71c847fd3d6b3b
lauslim12 <nicholasdwiarto@yahoo.com>
2020-07-20 17:56:23 +0700

    Tidied code with Black formatter

--- preparation.py
+++ preparation.py
@@ -12,47 +12,61 @@ import cv2
 WIDTH = 96
 HEIGHT = 96
 
-def conversion(character_name, cascade_file = os.path.join(os.getcwd(), 'data-models', 'lbpcascade_animeface.xml')):
-  # 1) Create a cascade file filled with the trained classifier from Nagadomi.
-  cascade = cv2.CascadeClassifier(cascade_file)
-  raw_images = os.listdir(os.path.join('dataset', 'raw', character_name))
 
-  # 2) Loop through the dataset/raw folder
-  for raw_image in raw_images:
-    # 3) Resize the image to be 96 x 96.
-    # First, read the image, then convert them into greyscale for easier computing.
-    image = cv2.imread(os.path.join('dataset', 'raw', character_name, raw_image))
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.equalizeHist(gray)
+def conversion(
+    character_name,
+    cascade_file=os.path.join(os.getcwd(), "data-models", "lbpcascade_animeface.xml"),
+):
+    # 1) Create a cascade file filled with the trained classifier from Nagadomi.
+    cascade = cv2.CascadeClassifier(cascade_file)
+    raw_images = os.listdir(os.path.join("dataset", "raw", character_name))
 
-    # Recognize the face in the image.
-    faces = cascade.detectMultiScale(gray,
-                                     scaleFactor=1.1,
-                                     minNeighbors=5,
-                                     minSize=(25, 25))
+    # 2) Loop through the dataset/raw folder
+    for raw_image in raw_images:
+        # 3) Resize the image to be 96 x 96.
+        # First, read the image, then convert them into greyscale for easier computing.
+        image = cv2.imread(os.path.join("dataset", "raw", character_name, raw_image))
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.equalizeHist(gray)
 
-    # Iterate through the face to crop.
-    for (x, y, w, h) in faces:
-      # crop_image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-      crop_image = image[y:y + h, x:x + w]
-      filename = os.path.basename(raw_image).split('.')[0]
-      cv2.imwrite(os.path.join('dataset', 'train', character_name, filename + '.jpg'), crop_image)
+        # Recognize the face in the image.
+        faces = cascade.detectMultiScale(
+            gray, scaleFactor=1.1, minNeighbors=5, minSize=(25, 25)
+        )
+
+        # Iterate through the face to crop.
+        for (x, y, w, h) in faces:
+            # crop_image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            crop_image = image[y : y + h, x : x + w]
+            filename = os.path.basename(raw_image).split(".")[0]
+            cv2.imwrite(
+                os.path.join("dataset", "train", character_name, filename + ".jpg"),
+                crop_image,
+            )
+
 
 def resize(character_name):
-  files = os.listdir(os.path.join('dataset', 'train', character_name))
+    files = os.listdir(os.path.join("dataset", "train", character_name))
 
-  for file in files:
-    image = cv2.imread(os.path.join('dataset', 'train', character_name, file))
-    resized_image = cv2.resize(image, (WIDTH, HEIGHT), interpolation = cv2.INTER_AREA)
-    cv2.imwrite(os.path.join('dataset', 'train', character_name, file), resized_image)
+    for file in files:
+        image = cv2.imread(os.path.join("dataset", "train", character_name, file))
+        resized_image = cv2.resize(image, (WIDTH, HEIGHT), interpolation=cv2.INTER_AREA)
+        cv2.imwrite(
+            os.path.join("dataset", "train", character_name, file), resized_image
+        )
+
 
 def main():
-  if len(sys.argv) != 2:
-    sys.stderr.write("Usage: python preparation.py <character_name> (character name must be a folder inside the 'raw' and 'train' folder, and must not contain spaces!) \n")
-    sys.exit(-1)
-  
-  conversion(sys.argv[1])
-  resize(sys.argv[1])
+    if len(sys.argv) != 2:
+        sys.stderr.write(
+            "Usage: python preparation.py <character_name> (character name must be a folder inside the 'raw' and 'train' folder, and must not contain spaces!) \n"
+        )
+        sys.exit(-1)
+
+    conversion(sys.argv[1])
+    resize(sys.argv[1])
+
 
 if __name__ == "__main__":
-  main()
+    main()
+
